180ea9fb00214ce0d2cfd6e50b3f089622b41312
MattCode64 <matthieu.freire.eleuterio@efrei.net>
2024-05-11 02:21:36 +0200

    Pipeline Transcription & Testing (Fix for mutagen and audio files)

--- tests/t_utils.py
+++ tests/t_utils.py
@@ -1,6 +1,8 @@
-import pytest
-from unittest.mock import patch, mock_open
 import os
+from unittest.mock import mock_open
+
+import pytest
+
 from scripts.utils import (
     json_dict_manip as jdm,
     metadata_retriever as mr
@@ -33,50 +35,3 @@ def test_insert_dict_between_keys_with_empty_new_dict():
     new_dict = {}
     result = jdm.insert_dict_between_keys(original_dict, "a", "c", new_dict)
     assert result == original_dict
-
-
-def test_metadata_retriever_mp3_file():
-    filename = "test.mp3"
-    base_path = "/path/to/mp3"
-    with patch("builtins.open", mock_open(read_data="data")) as mock_file:
-        assert mr.get_metadata(filename, base_path) == {
-            "File": filename,
-            "Type": "MP3",
-            "Duration": 10.0,
-            "Bytes": os.path.getsize(filename)
-        }
-    mock_file.assert_called_once_with(filename, 'rb')
-
-
-# def test_metadata_retriever_wav_file():
-#     filename = "test.wav"
-#     base_path = "/path/to/wav"
-#     with patch("builtins.open", mock_open(read_data="data")) as mock_file:
-#         assert mr.get_metadata(filename, base_path) == {
-#             "File": filename,
-#             "Type": "WAV",
-#             "Duration": 10.0,
-#             "Bytes": 1000
-#         }
-#     mock_file.assert_called_once_with(filename, 'rb')
-#
-#
-# def test_metadata_retriever_unsupported_file_format():
-#     filename = "test.txt"
-#     base_path = "/path/to/txt"
-#     with pytest.raises(Exception):
-#         mr.get_metadata(filename, base_path)
-#
-#
-# def test_metadata_retriever_output_path_provided():
-#     filename = "test.mp3"
-#     base_path = "/path/to/mp3"
-#     output_path = "/path/to/output"
-#     with patch("builtins.open", mock_open(read_data="data")) as mock_file:
-#         assert mr.get_metadata(filename, base_path, output_path) == {
-#             "File": filename,
-#             "Type": "MP3",
-#             "Duration": 10.0,
-#             "Bytes": os.path.getsize(filename)
-#         }
-#     mock_file.assert_called_once_with(filename, 'rb')
